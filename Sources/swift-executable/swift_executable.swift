cb6da8618c01b9a1dad05bfebcda63984c6e9fbf
Daksh Prajapati <msne0053@gmail.com>
2026-09-05 16:51:32 +0530

    Test

--- Sources/swift-executable/swift_executable.swift
+++ Sources/swift-executable/swift_executable.swift
@@ -1,9 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-@main
-struct swift_executable {
-    static func main() {
-        print("Hello, world!")
+import SwiftUI
+
+struct Cardview: View{
+    var body: some View{
+        VStack(alignment: .leading, spacing: 10){
+            Text("Hey!")
+                .font(.headline)
+
+            Text("This is my fitst real ios app")
+                .fontsize(100)
+        }
     }
 }
